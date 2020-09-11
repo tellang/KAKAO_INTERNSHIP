579a1be6d6e77cdd3d0a948ebc89f30db45da0d2
tellang <tellang@daum.net>
2020-09-11 16:44:39 +0900

    Backup

--- 2020_KAKAO_BLIND_RECRUITMENT/src/String_Compression/Solution.java
+++ 2020_KAKAO_BLIND_RECRUITMENT/src/String_Compression/Solution.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class Solution {
     public int solution(String s) {
-        int stringLen = s.length(), present, shortestLength = Integer.MAX_VALUE;
+        int stringLen = s.length(), present, shortestLength = stringLen;
         String sub;
         StringBuilder answer;
         Stack<String> stack = new Stack<>();
@@ -12,14 +12,13 @@ public class Solution {
             answer = new StringBuilder();
             present = 0;
             while (present < stringLen) {
-                if(stringLen - present < compressSize){
+                if (stringLen - present < compressSize) {
                     sub = s.substring(present);
                     answer.append(sub);
                     break;
-                }else if(stringLen - present == compressSize){
+                } else if (stringLen - present == compressSize) {
                     sub = s.substring(present);
-                }
-                else {
+                } else {
                     sub = s.substring(present, present + compressSize);
                 }
                 if (!stack.isEmpty()) {
@@ -28,10 +27,10 @@ public class Solution {
                             answer.append(stack.size());
                         }
                         stack.clear();
-                    }else {
-                        present+=compressSize;
+                    } else {
+                        present += compressSize;
                         stack.push(sub);
-                        if(present >= stringLen){
+                        if (present >= stringLen) {
                             answer.append(stack.size());
                         }
                         continue;
@@ -39,20 +38,22 @@ public class Solution {
                 }
                 stack.push(sub);
                 answer.append(stack.peek());
-                present+=compressSize;
+                present += compressSize;
             }
             stack.clear();
-            if(answer.length() < shortestLength){
+            if (answer.length() < shortestLength) {
                 shortestLength = answer.length();
+                System.out.println(answer+": "+answer.length()+" compressSize: "+compressSize);
             }
         }
         return shortestLength;
     }
 }
-class MainApp{
-    public static void main(String[]args){
+
+class MainApp {
+    public static void main(String[] args) {
         Solution s = new Solution();
-        String str = "ababcdcdababcdcd";
+        String str = "abcabcabcabcdededededede";
         System.out.println(s.solution(str));
     }
 }
